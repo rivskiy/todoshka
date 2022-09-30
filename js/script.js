cbaa2a229ac0dc7e3daa42639471e17103096ab9
Aleksey Pechurin <pe4urin@yandex.ru>
2022-09-30 12:52:26 +0300

    add localStorage and task quantity

--- js/script.js
+++ js/script.js
@@ -5,14 +5,20 @@ const doneList = document.querySelector('#doneList')
 const noTask = document.querySelector('#noTask');
 const noDone = document.querySelector('#noDone');
 
+let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+  tasks = JSON.parse(localStorage.getItem('tasks'))
+  tasks.forEach(task => {
+    renderTask(task)
+    checkNoTasks()
+    getQuantity()
+  });
+}
 
 form.addEventListener('submit', createTask);
-
 tasksList.addEventListener('click', removeTask);
-
 doneList.addEventListener('click', removeTask);
-
-
 tasksList.addEventListener('click', doneTask);
 
 function createTask(e) {
@@ -20,49 +26,95 @@ function createTask(e) {
   e.preventDefault();
 
   const taskText = taskInput.value
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    done: false
+  };
 
-  const taskHtml = `
-                    <li class="tasks__item">
-                      <span>${taskText}</span>
-                      <div class="tasks__btns">
-                        <button class="btn tasks__done-btn" type="button">Done</button>
-                        <button class="btn" type="button">Remove</button>
-                      </div>
-                    </li>
-                  `;
-  tasksList.insertAdjacentHTML('beforeend', taskHtml);
+  tasks.push(newTask)
+  saveToLocalStorage()
+
+  renderTask(newTask)
 
   taskInput.value = '';
   taskInput.focus();
 
-  if (tasksList.children.length > 1) {
-    noTask.style.display = 'none';
-  };
-
+  getQuantity()
+  checkNoTasks()
 };
 
 function removeTask(e) {
   if (e.target.innerHTML === 'Remove') {
     e.target.closest('.tasks__item').style.marginLeft = '100vw';
     setTimeout(() => {
+
+      const id = Number(e.target.closest('.tasks__item').id)
+
+      tasks = tasks.filter((task) => task.id !== id)
+      saveToLocalStorage()
       e.target.closest('.tasks__item').remove();
-      if (tasksList.children.length === 1) {
-        noTask.style.display = 'block';
-      };
-      if (doneList.children.length === 1) {
-        noDone.style.display = 'block';
-      };
+
+      getQuantity()
+      checkNoTasks()
     }, 200)
   };
 };
 
 function doneTask(e) {
   if (e.target.innerHTML === 'Done') {
+    const id = Number(e.target.closest('.tasks__item').id)
+    const task = tasks.find((task) => task.id === id)
+    task.done = !task.done
+    saveToLocalStorage()
+
     doneList.appendChild(e.target.closest('.tasks__item'));
     doneList.querySelector('.tasks__done-btn').remove();
   };
-  if (doneList.children.length > 1) {
-    noDone.style.display = 'none';
-  };
+
+  getQuantity()
+  checkNoTasks()
 };
 
+function checkNoTasks() {
+
+  if (tasksList.children.length !== 1) {
+    noTask.style.display = 'none';
+  } else {
+    noTask.style.display = 'block';
+  };
+
+  if (doneList.children.length !== 1) {
+    noDone.style.display = 'none';
+  } else {
+    noDone.style.display = 'block';
+  }
+};
+
+function  getQuantity() {
+  const taskQuantity = document.querySelector('#taskQuantity');
+  taskQuantity.innerHTML = tasksList.children.length - 1
+  const doneQuantity = document.querySelector('#doneQuantity')
+  doneQuantity.innerHTML = doneList.children.length - 1
+  // const taskQuantity = document.querySelector('#taskQuantity');
+  // taskQuantity.innerHTML = tasks.length
+  // const doneQuantity = document.querySelector('#doneQuantity')
+  // doneQuantity.innerHTML = doneList.children.length - 1
+};
+
+function saveToLocalStorage() {
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function renderTask(task) {
+  const taskHtml = `
+  <li class="tasks__item" id="${task.id}">
+    <span>${task.text}</span>
+    <div class="tasks__btns">
+      <button class="btn tasks__done-btn" type="button">Done</button>
+      <button class="btn" type="button">Remove</button>
+    </div>
+  </li>
+`;
+tasksList.insertAdjacentHTML('beforeend', taskHtml);
+}
